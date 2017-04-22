62697f12940b96240bccfdecb04bfd9bf2752842
Anil <anilkumar.kotur@go-jek.com>
2017-04-22 12:05:07 +0530

    segmentcontroller and its action

--- FarmshipIOS/FarmshipIOS/ViewController.swift
+++ FarmshipIOS/FarmshipIOS/ViewController.swift
@@ -10,10 +10,23 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    
+    @IBOutlet weak var segmentController: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
     }
-
+    
+    @IBAction func segmentAction(_ sender: Any) {
+        if self.segmentController.selectedSegmentIndex == 0 {
+            print(self.segmentController.selectedSegmentIndex)
+        }
+        else if self.segmentController.selectedSegmentIndex == 1 {
+            print(self.segmentController.selectedSegmentIndex)
+        }
+        else if self.segmentController.selectedSegmentIndex == 2 {
+            print(self.segmentController.selectedSegmentIndex)
+        }
+    }
 }
 
