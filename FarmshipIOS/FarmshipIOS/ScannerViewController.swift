2f387b72052454612478c84ce1b6ab0781b8a372
Anil <anilkumar.kotur@go-jek.com>
2017-04-22 22:06:15 +0530

    motion update to firbase

--- FarmshipIOS/FarmshipIOS/ScannerViewController.swift
+++ FarmshipIOS/FarmshipIOS/ScannerViewController.swift
@@ -14,7 +14,7 @@ import Firebase
 class ScannerViewController: UIViewController {
     
     
-    let dataBaseRef = FIR
+    let dataBaseRef = FIRDatabase.database().reference().child("controller/1/direction")
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,37 +23,48 @@ class ScannerViewController: UIViewController {
     
     @IBAction func leftButtonDidTouch(_ sender: Any) {
         print("left on touch")
+        self.upDateDbOnActions(value: "a")
     }
-    
     
     @IBAction func leftButtonOnRelase(_ sender: Any) {
         print("left on release")
+        self.upDateDbOnActions(value: "x")
     }
 
     
     @IBAction func upButtonDidTouch(_ sender: Any) {
         print("up on touch")
+        self.upDateDbOnActions(value: "w")
     }
     
     @IBAction func upButtonOnRelease(_ sender: Any) {
         print("up on release")
+        self.upDateDbOnActions(value: "x")
     }
     
     @IBAction func rightButtonDidTouch(_ sender: Any) {
-     print("right on touch")
+        print("right on touch")
+        self.upDateDbOnActions(value: "s")
     }
     
     @IBAction func rightButtonOnRelease(_ sender: Any) {
         print("right on release")
+        self.upDateDbOnActions(value: "x")
     }
     
     @IBAction func downButtonDidTouch(_ sender: Any) {
         print("down on touch")
+        self.upDateDbOnActions(value: "d")
     }
     
     @IBAction func downButtonOnrelease(_ sender: Any) {
         print("down on release")
+        self.upDateDbOnActions(value: "x")
     }
     
+    
+    func upDateDbOnActions(value: String)  {
+        dataBaseRef.setValue(value)
+    }
     
 }
