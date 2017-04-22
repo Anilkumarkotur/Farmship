1694f7be43b080a1c3bf9c865e7635e2e52eeec3
Anil <anilkumar.kotur@go-jek.com>
2017-04-22 13:10:53 +0530

    navbar and segment design

--- FarmshipIOS/FarmshipIOS/ViewController.swift
+++ FarmshipIOS/FarmshipIOS/ViewController.swift
@@ -9,14 +9,17 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
     
     @IBOutlet weak var segmentController: UISegmentedControl!
+    @IBOutlet weak var segmentView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.navBarSetUp()
+        self.segmentViewSetup()
     }
     
+    //MARK: Actions  
     @IBAction func segmentAction(_ sender: Any) {
         if self.segmentController.selectedSegmentIndex == 0 {
             print(self.segmentController.selectedSegmentIndex)
@@ -28,5 +31,33 @@ class HomeViewController: UIViewController {
             print(self.segmentController.selectedSegmentIndex)
         }
     }
+    
+    //MARK: UI Helper
+    func navBarSetUp() {
+        let titleLable = UILabel(frame: CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: 20))
+        titleLable.font = UIFont.neoSansProBold(size: 16)
+        titleLable.text = "Farmship"
+        titleLable.textAlignment = .center
+        titleLable.textColor = UIColor.white
+        self.navigationItem.titleView = titleLable
+        self.navigationController?.navigationBar.barStyle = UIBarStyle.black
+        self.navigationController?.navigationBar.isTranslucent = false
+        self.navigationController?.navigationBar.barTintColor = UIColor.farmshipColor()
+        self.navigationController?.navigationBar.tintColor = UIColor.farmshipColor()
+        self.navigationController?.navigationBar.backgroundColor = UIColor.farmshipColor()
+    }
+    
+    func segmentViewSetup() {
+        segmentController.setTitleTextAttributes([NSFontAttributeName: UIFont.neoSansProMedium(size: 12) as Any], for: .normal)
+        segmentView.layer.masksToBounds = false
+        segmentView.layer.shadowColor = UIColor.black.cgColor
+        segmentView.layer.shadowOffset = CGSize(width: 5, height: 5)
+        segmentView.layer.shadowOpacity = 0.25
+        segmentView.layer.shadowRadius = 10.0
+        segmentView.layer.masksToBounds = true
+    }
+    
+
 }
+
 
---
+++ FarmshipIOS/FarmshipIOS/utilites/UIColorHelper.swift
@@ -0,0 +1,33 @@
+//
+//  UIColorHelper.swift
+//  FarmshipIOS
+//
+//  Created by Anilkumar kotur on 22/04/17.
+//  Copyright © 2017 Anilkumar kotur. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    // MARK: App navbar color /Images etc
+    class func farmshipColor() -> UIColor {
+        return UIColor(red:0.30, green:0.20, blue:0.10, alpha:1.00)
+    }
+    
+}
+
+extension UIFont {
+    class func neoSansProRegular(size: CGFloat) -> UIFont? {
+        return UIFont(name: "HelveticaNeue-Regular", size: size)
+    }
+
+    class func neoSansProMedium(size: CGFloat) -> UIFont? {
+        return UIFont(name: "HelveticaNeue-Medium", size: size)
+    }
+
+    class func neoSansProBold(size: CGFloat) -> UIFont? {
+        return UIFont(name: "HelveticaNeue-Bold", size: size)
+    }
+
+}
